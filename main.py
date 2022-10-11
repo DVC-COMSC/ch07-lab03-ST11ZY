2f8e918567e9d3422979cf395cc6dc27cc1e8abf
Your Name <you@example.com>
2022-10-10 21:36:18 -0700

    ch3

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-
-# ******************************
-# Make your Code
-# ******************************
+numbers=[]
+x=0
+for i in range (5):
+    numbers.append(int(input("input?")))
+total=sum(numbers)
+avg=total/5
+while x<5:
+    if numbers[x]>avg:
+        print(numbers[x])
+    x=x+1
+# Make your Code******************************
